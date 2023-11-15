a322d64ea97f7ff485d6f911bd29485e531aa94c
jendaaaaa <143932605+jendaaaaa@users.noreply.github.com>
2023-11-15 17:28:30 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,22 @@
 // GROUP MB3-B
-// moving wall W2, 3, switch S2, color C3, display D2
+// moving wall W2, switch S2, display D2
+// radio from RESET
+// radio from MB-C
 
 // INIT
 let PIN_SCL = DigitalPin.P19;
 let PIN_SDA = DigitalPin.P20;
 let PIN_SERVO = AnalogPin.P0;
 let PIN_BUTTON = DigitalPin.P1;
+let PIN_SWITCH_OPEN = DigitalPin.P8;
+let PIN_SWITCH_CLOSE = DigitalPin.P9;
 
 radio.setGroup(8);
 led.enable(false);
 let displaySegment = TM1637.create(PIN_SCL, PIN_SDA, 10, 4);
 pins.setPull(PIN_BUTTON, PinPullMode.PullUp);
+pins.setPull(PIN_SWITCH_CLOSE, PinPullMode.PullUp);
+pins.setPull(PIN_SWITCH_OPEN, PinPullMode.PullUp);
 
 // DEBOUNCING
 let buttonState = 0;
@@ -24,16 +30,18 @@ let TIME_DEBOUNCE = 0;
 let PIN_PRESSED = 0;
 let PIN_RELEASED = 1;
 
-// VARIABLES
-let value = 0;
-let servo = 0;
-
+// CONSTANTS
 let TIME_TO_OPEN = 5000;
-let SERVO_OPEN = 85;
-let SERVO_CLOSE = 10;
-
+let SERVO_STOP = 90
+let SERVO_SPEED = 70;
+let SERVO_OPEN = SERVO_STOP + SERVO_SPEED;
+let SERVO_CLOSE = SERVO_STOP - SERVO_SPEED;
 let RADIO_RESET = 1;
 let RADIO_CLOSE = 2;
+
+// VARIABLES
+let servoSpeed = SERVO_STOP;
+let colorIsCorrect = false;
 
 // INTERRUPT
 radio.onReceivedNumber(function (receivedNumber) {
@@ -44,18 +52,35 @@ radio.onReceivedNumber(function (receivedNumber) {
     }
 })
 
+radio.onReceivedValue(function(name: string, value: number) {
+    if (name === "C3"){
+        if (value === 1){
+            colorIsCorrect = true;
+        } else if (value === 0) {
+            colorIsCorrect = false;
+        }
+    }
+})
+
 // MAIN
 basic.forever(function () {
     debounceButton();
+    // color sensor C3
+    if (colorIsCorrect){
+        gateOpen();
+    } else {
+        gateClose();
+    }
+    // switch timer S2
     if (isMeasured) {
         if (input.runningTime() - timeFromPress > TIME_TO_OPEN) {
-            servo = SERVO_OPEN;
+            gateClose();
             isMeasured = false;
         } else {
-            servo = SERVO_CLOSE;
+            gateOpen();
         }
     }
-    pins.servoWritePin(PIN_SERVO, servo);
+    pins.servoWritePin(PIN_SERVO, servoSpeed);
 })
 
 basic.forever(function () {
@@ -87,6 +112,24 @@ function debounceButton () {
 }
 
 function resetState() {
-    servo = SERVO_CLOSE;
+    gateClose();
     timeElapsed = 0;
+}
+
+//////////////////////////////////////////////////////////////////
+
+function gateClose() {
+    if (pins.digitalReadPin(PIN_SWITCH_OPEN)) {
+        servoSpeed = SERVO_CLOSE;
+    } else {
+        servoSpeed = SERVO_STOP;
+    }
+}
+
+function gateOpen() {
+    if (pins.digitalReadPin(PIN_SWITCH_CLOSE)) {
+        servoSpeed = SERVO_OPEN;
+    } else {
+        servoSpeed = SERVO_STOP;
+    }
 }
